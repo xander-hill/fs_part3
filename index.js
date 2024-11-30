87d31eb1a87aedd49b9df9471e29e886dfb13205
Xander Hill <xander.k.hill@gmail.com>
2024-11-29 21:44:06 -0600

    Validation error handling

--- index.js
+++ index.js
@@ -142,6 +142,9 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        console.log(error.message)
+        return response.status(400).send({error: error.message})
     }
 
     next(error)
